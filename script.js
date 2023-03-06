d758b4a7d8e8808760eaa3318f7e644b2c9b23ba
mjj6514 <99623496+mjj6514@users.noreply.github.com>
2023-03-06 18:50:34 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
-function a()
-{
-    let ans = prompt("관리자 권한");
-alert(ans)
-if (ans == "SW_!#@%")
-{
-    location.href='indexX.html';
+
+
+function passWord() {
+
+    var pass1 = prompt('암호를 입력하십시오','암호를 입력하세요');
+
+    while (true) {
+
+        if (!pass1)
+
+    history.go(-1);
+
+    if (pass1.toLowerCase() == "sw1325")
+    {
+    alert('어서 오세요! 확인을 누르면 지정하신 페이지로 이동합니다.');
+
+    location.href ='indexX.html';
+
+    break;
+
+    }else{    prompt('암호를 확인 하시고 다시 입력하세요!.','암호 확인');
+
 }
+
+if (pass1.toLowerCase()!="password" & testV ==3)
+
+history.go(-1);
+    }
+
+
+
+
 }
